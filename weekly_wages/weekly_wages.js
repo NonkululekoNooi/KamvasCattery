5c51a96fff9669e1079c0a08b171082ea7c8dbf4
NonkululekoNooi <nooi9423@gmail.com>
2022-04-22 15:44:48 +0200

    I have created files here

--- weekly_wages/weekly_wages.js
+++ weekly_wages/weekly_wages.js
@@ -1,3 +1,26 @@
 function weeklyWage(band, weeklyHours) {
 
-};
+    var currentList = []
+
+    for(var i=0; i<band.length; i++){
+
+        var current = band[i]
+
+        if(weeklyHours == band){
+            return 483
+        }
+        else if(weeklyHours == band){
+            return 1221
+        }
+        else if(weeklyHours == band ){
+            return 1836
+        }
+        else if(weeklyHours < '7'){
+            return 'missing day'
+        }
+        else if(weeklyHours != band) {
+            return 'missing day'
+        }
+    }
+}
+
